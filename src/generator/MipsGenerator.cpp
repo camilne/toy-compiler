edbed40b19489de3bef9628da998bb59e29f5e4b
Cameron Milne <cameronm213@gmail.com>
2018-03-13 16:31:42 -0600

    Moved iterator increment in optimize to loop

--- src/generator/MipsGenerator.cpp
+++ src/generator/MipsGenerator.cpp
@@ -102,29 +102,25 @@ void MipsGenerator::optimize() {
     for(auto& i : popRegisters)
         i = 0;
 
-    for(auto it = mipsStatements.rbegin(); it != mipsStatements.rend(); /* Empty */) {
+    for(auto it = mipsStatements.rbegin(); it != mipsStatements.rend(); ++it) {
         std::shared_ptr<MipsStatement> obj = *it;
         if(std::shared_ptr<MipsPop> pop = std::dynamic_pointer_cast<MipsPop>(obj)) {
             if(pop->getRegister() < popRegisters.size()) {
                 if(!popRegisters[pop->getRegister()]) {
                     mipsStatements.erase(std::next(it).base());
+                    --it;
                 } else {
                     --popRegisters[pop->getRegister()];
-                    ++it;
                 }
             }
         } else if(std::shared_ptr<MipsPush> push = std::dynamic_pointer_cast<MipsPush>(obj)) {
             if(push->getRegister() < popRegisters.size())
                 ++popRegisters[push->getRegister()];
-            ++it;
         } else if(std::shared_ptr<MipsOp> op = std::dynamic_pointer_cast<MipsOp>(obj)) {
             if(op->getArg1Register() < popRegisters.size())
                 ++popRegisters[op->getArg1Register()];
             if(op->getArg2Register() < popRegisters.size())
                 ++popRegisters[op->getArg2Register()];
-            ++it;
-        } else {
-            ++it;
         }
     }
 }
