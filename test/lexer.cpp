478327be38829113d8150c837e787b03eac4e5dd
camilne <cameronm213@gmail.com>
2018-03-17 21:36:41 -0600

    Added lexer integer testing

--- test/lexer.cpp
+++ test/lexer.cpp
@@ -2,10 +2,13 @@
 #include "../generated/grammar.tab.h"
 #include <vector>
 #include <string>
+#include <array>
 
 struct yy_buffer_state;
 typedef struct yy_buffer_state *YY_BUFFER_STATE;
 extern YY_BUFFER_STATE yy_scan_string(const char* str);
+union YYSTYPE;
+extern YYSTYPE yylval;
 extern int yylex();
 
 std::vector<int> lexString(const std::string& str) {
@@ -77,5 +80,28 @@ TEST_CASE("ignores whitespace", "[lexer]") {
     std::vector<int> tokens = lexString(source);
 
     REQUIRE(refTokens == tokens);
+  }
+}
+
+TEST_CASE("tokenizes integers", "[lexer]") {
+  static const std::string source = "0 1 12 015 1234567890";
+
+  SECTION("type") {
+    std::vector<int> tokens = lexString(source);
+
+    REQUIRE(tokens.size() == 5);
+    REQUIRE(tokens[0] == yytokentype::INTEGER);
+    REQUIRE(tokens[1] == yytokentype::INTEGER);
+    REQUIRE(tokens[2] == yytokentype::INTEGER);
+    REQUIRE(tokens[3] == yytokentype::INTEGER);
+    REQUIRE(tokens[4] == yytokentype::INTEGER);
+  }
+
+  SECTION("value") {
+    yy_scan_string(source.c_str());
+    std::array<int, 5> values = {0, 1, 12, 15, 1234567890};
+    for(auto it = values.begin(); it != values.end() && yylex(); ++it) {
+      REQUIRE(*it == yylval.INT);
+    }
   }
 }
