e88577db1c30edd3605e7f5b4a470f83d130efca
camilne <cameronm213@gmail.com>
2018-03-21 10:17:23 -0600

    Added transparent parentheses tests

--- test/grammar.cpp
+++ test/grammar.cpp
@@ -62,33 +62,6 @@ TEST_CASE("parses statement", "[parser]") {
     REQUIRE(printNode->getExp());
   }
 
-  SECTION("integer literal") {
-    static const std::string source = "10;";
-
-    auto ast = parseString(source);
-
-    REQUIRE(ast);
-    REQUIRE(ast->getRoot());
-    REQUIRE(ast->getRoot()->getStatements());
-    auto integerNode = ast->getRoot()->getStatements()->getStatementAs<IntegerNode*>();
-    REQUIRE(integerNode);
-    REQUIRE(integerNode->getValue() == 10);
-  }
-
-  SECTION("identifier") {
-    static const std::string source = "id;";
-    static const std::string idName = "id";
-
-    auto ast = parseString(source);
-
-    REQUIRE(ast);
-    REQUIRE(ast->getRoot());
-    REQUIRE(ast->getRoot()->getStatements());
-    auto identifierNode = ast->getRoot()->getStatements()->getStatementAs<IdentifierNode*>();
-    REQUIRE(identifierNode);
-    REQUIRE(identifierNode->getName() == idName);
-  }
-
   SECTION("empty while") {
     static const std::string source = "while(0){}";
 
@@ -119,6 +92,33 @@ TEST_CASE("parses statement", "[parser]") {
 }
 
 TEST_CASE("parses expression", "[parser]") {
+  SECTION("integer literal") {
+    static const std::string source = "10;";
+
+    auto ast = parseString(source);
+
+    REQUIRE(ast);
+    REQUIRE(ast->getRoot());
+    REQUIRE(ast->getRoot()->getStatements());
+    auto integerNode = ast->getRoot()->getStatements()->getStatementAs<IntegerNode*>();
+    REQUIRE(integerNode);
+    REQUIRE(integerNode->getValue() == 10);
+  }
+
+  SECTION("identifier") {
+    static const std::string source = "id;";
+    static const std::string idName = "id";
+
+    auto ast = parseString(source);
+
+    REQUIRE(ast);
+    REQUIRE(ast->getRoot());
+    REQUIRE(ast->getRoot()->getStatements());
+    auto identifierNode = ast->getRoot()->getStatements()->getStatementAs<IdentifierNode*>();
+    REQUIRE(identifierNode);
+    REQUIRE(identifierNode->getName() == idName);
+  }
+
   SECTION("unary minus") {
     static const std::string source = "-5;";
 
@@ -209,5 +209,18 @@ TEST_CASE("parses expression", "[parser]") {
     REQUIRE(assignmentNode);
     REQUIRE(assignmentNode->getIdentifier());
     REQUIRE(assignmentNode->getExpression());
+  }
+
+  SECTION("single parentheses are transparent") {
+    static const std::string source = "(1);";
+
+    auto ast = parseString(source);
+
+    REQUIRE(ast);
+    REQUIRE(ast->getRoot());
+    REQUIRE(ast->getRoot()->getStatements());
+    auto integerNode = ast->getRoot()->getStatements()->getStatementAs<IntegerNode*>();
+    REQUIRE(integerNode);
+    REQUIRE(integerNode->getValue() == 1);
   }
 }
